092c86f85f640463b4fe99b18c3d366a84c403b5
liujing <liujing@163.com>
2019-04-21 16:25:17 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 引入当前项目的全局css样式控制文件
+import './assets/css/global.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
